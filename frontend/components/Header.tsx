b5ccf8d749fe661d5cc822c79528cb7944308f7c
shyu61 <ohs6261@gmail.com>
2020-12-31 00:19:54 +0900

    Create Header

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/common.module.scss';
+
+const Header = () => {
+  return <div className={styles.header}>Header</div>;
+};
+
+export default Header;
